2412e731786c3d0de5decf56c162ba02f85a6311
Idris <idris.maps@gmx.ch>
2016-02-27 11:46:44 +0100

    script in HTML file

--- ui/client-lib/pic.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(src) {
-	$('#list').hide()
-	$('#pic').html('<img src="' + src + '"><br><button id="back">Back to list</button>')
-	$('#pic').show()
-	$('#back').on('click', function() {
-		$('#list').show()
-	}) 
-}
